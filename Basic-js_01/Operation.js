df14ca3387febeebe5407832cb81fd668c503581
Ramya Menon <149166400+MenonRamya@users.noreply.github.com>
2024-07-18 12:35:59 +0000

    Operations

---
+++ Basic-js_01/Operation.js
@@ -0,0 +1,32 @@
+let value=3
+let negval=-value
+//console.log(negval);
+
+//console.log(2+3);
+//console.log(2-3);
+//console.log(2*3);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+
+let str1="hello"
+let str2=" ramya"
+//console.log(str1+str2) this is concatination
+
+/*COMPARISON*/ 
+
+console.log(2>1) 
+//2<1 2==1 2>=1 2=<1
+//Note : two datatypes shouldnt be compared 
+
+/* Example
+console.log(null>=0) true
+console.log(null == 0 ) false
+this is because comparison operator converts the null to a number and thus it comes as true 
+
+AVOID THIS TYPE OF COMPARISONS OF UNDEFINED AND NULL
+*/
+
+//===
+
+console.log("2"===2)
